d5f7c2ccc67fe07bc956cec700c550150df9ffb3
pain103190@gmail.com <124682191+judah2023@users.noreply.github.com>
2023-07-19 19:47:54 +0900

    (2023.7.19) 자료형 변환

---
+++ Project1/Project1/(2023.7.18) 반복문.h
@@ -0,0 +1,175 @@
+#pragma once
+#include <stdio.h>
+
+void main()
+{
+#pragma region switch 문
+	// 어떤 결과에 따라 그 결과부터 실행되는 명령문입니다.
+
+	// switch 문의 경우 조건에 해당하는 값에 따라 조건의 위치로 이동합니다.
+
+	//	int input = 5;
+	//	char grade = 'A';
+	//	float exp = 5.75f;
+	//	
+	//	// switch 문에는 조건식으로 실수형 변수와 실수형 상수를 사용할 수 없습니다.
+	//	
+	//	switch (grade)
+	//	{
+	//		// break 문이란?
+	//		// 특정한 지점에서 분기를 탈출하는 제어문입니다.
+	//	case 'A': printf("case 1\n");
+	//		break;
+	//	case 'B': printf("case 2\n");
+	//		break;
+	//	case 'C': printf("case 3\n");
+	//		break;
+	//	default: printf("알 수 없는 값입니다.\n");
+	//		break;
+	//	}
+	// switch 문은 해당 조건이 완료되었을 때, break 문이 없으면 나머지 밑에 있는 case 문까지 계속 실행되다가 종료합니다.
+
+
+
+#pragma endregion
+
+#pragma region 반복문
+	// 프로그램 내에서 특정한 작업을 반복적으로 수행하는 명령문입니다.
+
+	// 증감 연산자란?
+	// 피연산자를 하나씩 증가시키거나 감소시킬 때 사용하는 연산자입니다.
+
+	// 전위 증감 연산자
+	// 변수의 값을 증감시킨 후에 연산을 수행합니다.
+	//	int x = 0;
+	//	int y = 0;
+	//	
+	//	x = ++y;
+	//
+	// printf("x의 값은 : %d, y의 값 : %d\n", x, y);
+	//
+	// 후위 증감 연산자
+	// 대입 연산을 수행한 다음 변수의 값을 증감시킵니다.
+	//
+	//	int damage = 10;
+	//	
+	//	int health = damage--;
+	//	
+	//	printf("health의 값 : %d\n", health);
+	//	printf("damage의 값 : %d\n", damage);
+	//
+	// 후위 증감 연산자의 경우 변화되는 값을 나중에 가지고 오기 때문에 추가적인 메모리가 필요합니다.
+
+	// for 문이란?
+	/*
+	// 초기식을 연산하여 조건식의 결과에 따라 특정한 횟수만큼 반복하는 반복문입니다.
+
+	// 1. (초기식) int i = 0;
+	// 2. (조건식) i < 5
+	// 3. (반복할 내용) printf("for문\n");
+	// 4. (증감식) i++;
+	//	for (int i = 0; i < 5; i++)
+	//	{
+	//		printf("for문\n");
+	//	}
+
+	// for 문의 경우 조건이 끝나는 형태와 반대로 초기식을 연산하게 되면 조건이 일치하지 않아 계속 반복적으로 실행되는 문제가 발생합니다.
+	*/
+
+	// for 문을 이용하여 초기식으로 1 ~ 10을 출력해주세요.
+	/*
+	//	for (int s = 1; s < 11; s++)
+	//	{
+	//		printf("s의 값 : %d\n", s);
+	//	}
+
+	// 내림차순
+	for (int y = 10; y >= 1; y--)
+	{
+		printf("y의 값 : %d\n", y);
+	}
+	*/
+
+	// 복합 대입 연산자
+	/*
+	// +=, -=, *=, /=, %=
+
+	int data = 10;
+
+	data += 10;
+	printf("data에 '+=' 한 결과 : %d\n", data);
+
+	data -= 5;
+	printf("data에 '-=' 한 결과 : %d\n", data);
+
+	data *= 3;
+	printf("data에 '*=' 한 결과 : %d\n", data);
+
+	data /= -5;
+	printf("data에 '+=' 한 결과 : %d\n", data);
+	*/
+
+	// 2중 for문
+	/*
+for (int i = 0; i < 3; i++)
+{
+	printf("밖에 있는 for문\n");
+
+	for (int j = 0; j < 3; j++)
+	{
+		printf("안쪽에 있는 for문\n");
+	}
+
+
+
+}
+*/
+
+// 구구단
+/*
+ for (int i = 1; i < 10; i++)
+ {
+	 for (int j = 1; j < 10; j++)
+	 {
+		 printf("%d * %d : %d\n", i, j, i * j);
+	 }
+	 printf("\n");
+ }
+ */
+
+ // while 문
+ /*
+ // 특정 조건을 만족할 때까지 계속해서 주어진 명령문을 실행하는 반복문입니다.
+
+ int count = 5;
+
+ while(count)
+ {
+	 printf("실행 중....\n");
+	 count--;
+ }
+
+ while (1)
+ {
+	 // 키 입력
+
+	 // 시간에 따라 자동으로 움직이는 오브젝트
+ }
+ */
+
+ // do-while 문
+ /*
+ // 조건과 상관없이 한번의 작업을 수행한 다음 조건에 따라 명령문을 실행하는 반복문입니다.
+
+ do
+ {
+	 printf("게임 시작\n");
+ } while (10 < 20);
+ */
+
+
+
+
+#pragma endregion
+
+}
--- Project1/Project1/Source1.cpp
+++ Project1/Project1/Source1.cpp
@@ -2,136 +2,85 @@
 
 void main()
 {
-#pragma region switch 문
-	// 어떤 결과에 따라 그 결과부터 실행되는 명령문입니다.
+#pragma region 자료형 변환
+	// 서로 다른 자료형을 가지고 있는 변수끼리 연산이 이루어질 때 기존에 지정했던 자료형을 다른 자료형으로 변환하는 과정입니다.
 
-	// switch 문의 경우 조건에 해당하는 값에 따라 조건의 위치로 이동합니다.
+	// 암묵적 형 변환
+	/*
+	// 서로 다른 자료형으로 연산이 이루어질 때 자료형의 크기가 큰 자료형으로 변환되는 과정입니다.
 
-	//	int input = 5;
-	//	char grade = 'A';
-	//	float exp = 5.75f;
-	//	
-	//	// switch 문에는 조건식으로 실수형 변수와 실수형 상수를 사용할 수 없습니다.
-	//	
-	//	switch (grade)
-	//	{
-	//		// break 문이란?
-	//		// 특정한 지점에서 분기를 탈출하는 제어문입니다.
-	//	case 'A': printf("case 1\n");
-	//		break;
-	//	case 'B': printf("case 2\n");
-	//		break;
-	//	case 'C': printf("case 3\n");
-	//		break;
-	//	default: printf("알 수 없는 값입니다.\n");
-	//		break;
-	//	}
-	// switch 문은 해당 조건이 완료되었을 때, break 문이 없으면 나머지 밑에 있는 case 문까지 계속 실행되다가 종료합니다.
+	int integer = 10;
+	float decimal = 6.5f;
 
 
+	// sizeof() : 자료형의 크기를 반환하는 연산자입니다.
+	// printf("int의 자료형의 크기 = %d\n", sizeof(int));
+	// printf("float의 자료형의 크기 = %d\n", sizeof(int));
+	// 
+	// printf("integer + decimal = %f\n", integer + decimal);
 
-#pragma endregion
+	integer = 7.756f;
 
-#pragma region 반복문
-	// 프로그램 내에서 특정한 작업을 반복적으로 수행하는 명령문입니다.
-
-	// 증감 연산자란?
-	// 피연산자를 하나씩 증가시키거나 감소시킬 때 사용하는 연산자입니다.
-
-	// 전위 증감 연산자
-	// 변수의 값을 증감시킨 후에 연산을 수행합니다.
-	//	int x = 0;
-	//	int y = 0;
-	//	
-	//	x = ++y;
-	//
-	// printf("x의 값은 : %d, y의 값 : %d\n", x, y);
-	//
-	// 후위 증감 연산자
-	// 대입 연산을 수행한 다음 변수의 값을 증감시킵니다.
-	//
-	//	int damage = 10;
-	//	
-	//	int health = damage--;
-	//	
-	//	printf("health의 값 : %d\n", health);
-	//	printf("damage의 값 : %d\n", damage);
-	//
-	// 후위 증감 연산자의 경우 변화되는 값을 나중에 가지고 오기 때문에 추가적인 메모리가 필요합니다.
+	// 표현범위가 작은 데이터에 표현범위가 큰 데이터를 저장하게 되면 암묵적으로 데이터 손실이 발생합니다.
+	printf("integer 변수의 값 = %d\n", integer);
 	
-	// for 문이란?
-	/*
-	// 초기식을 연산하여 조건식의 결과에 따라 특정한 횟수만큼 반복하는 반복문입니다.
-
-	// 1. (초기식) int i = 0;
-	// 2. (조건식) i < 5
-	// 3. (반복할 내용) printf("for문\n");
-	// 4. (증감식) i++;
-	//	for (int i = 0; i < 5; i++)
-	//	{
-	//		printf("for문\n");
-	//	}
-
-	// for 문의 경우 조건이 끝나는 형태와 반대로 초기식을 연산하게 되면 조건이 일치하지 않아 계속 반복적으로 실행되는 문제가 발생합니다.
+	// 정수형에서 실수형으로 암묵적 자료형 변환은 가능하지만, 실수형에서 정수형으로 암묵적 자료형 변환은 되지 않습니다.
 	*/
 
-	// for 문을 이용하여 초기식으로 1 ~ 10을 출력해주세요.
-	/*
-	//	for (int s = 1; s < 11; s++)
-	//	{
-	//		printf("s의 값 : %d\n", s);
-	//	}
+	// 명시적 형 변환
+	// 연산이 이루어지기 전에 사용자가 직접 자료형을 변환하는 과정입니다.
 
-	// 내림차순
-	for (int y = 10; y >= 1; y--)
-	{
-		printf("y의 값 : %d\n", y);
-	}
-	*/
+	int x = 10;
+	int y = 3;
 
-	// 복합 대입 연산자
-	/*
-	// +=, -=, *=, /=, %=
+	// 정수형 변수끼리 연산을 수행하게 되면 정수의 결과 값만 가질 수 있습니다.
+	float result = (float)x / y;
 
-	int data = 10;
-
-	data += 10;
-	printf("data에 '+=' 한 결과 : %d\n", data);
-
-	data -= 5;
-	printf("data에 '-=' 한 결과 : %d\n", data);
-
-	data *= 3;
-	printf("data에 '*=' 한 결과 : %d\n", data);
-
-	data /= -5;
-	printf("data에 '+=' 한 결과 : %d\n", data);
-	*/
-
-	// 2중 for문
-/*
-for (int i = 0; i < 3; i++)
-{
-	printf("밖에 있는 for문\n");
-
-	for (int j = 0; j < 3; j++)
-	{
-		printf("안쪽에 있는 for문\n");
-	}
-
-
-
-}
-*/
-
-	// 구구단
-
-
+	
+	// printf("result의 값 : %f\n", result);
 
 
 
 
 
 #pragma endregion
+
+#pragma region continue문
+	// 해당 조건문만 실행하지 않고, 반복문은 이어서 실행하는 제어문입니다.
+
+	/*
+	for (int i = 1; i <= 5; i++)
+	{
+		if (i == 3)
+		{
+			continue;
+		}
+		printf("i의 값 : %d\n", i);
+
+	}
+	*/
+
+
+
+
+
+#pragma endregion
+
+#pragma region 시프트 연산자
+	// 비트 값을 주어진 숫자만큼 부호 방향으로 이동시키는 연산자입니다.
+
+	// int a = 3; // 0000 0011
+	// 
+	// printf("a를 왼쪽 2번 시프트 연산한 결과 : %d\n", a <<= 2); // 0000 1100
+	// 
+	// printf("a의 값 : %d\n", a); // 0000 1100
+	// 
+	// printf("a를 오른쪽 3번 시프트 연산한 결과 : %d\n", a >> 3); // 0000 0001
+
+
+
+
+#pragma endregion
+
 
 }
