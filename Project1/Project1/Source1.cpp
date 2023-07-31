38da30e327dd1e1a235a4813f903e12a66d00893
pain103190@gmail.com <pain103190@gmail.com>
2023-07-31 19:46:46 +0900

    (2023.7.31) 동적 할당

--- Project1/Project1/Source1.cpp
+++ Project1/Project1/Source1.cpp
@@ -2,10 +2,12 @@
 
 int main()
 {
-	char a[51];
-	scanf_s("%s", &a, 51);
-	printf("%s??!", a);
+	long A, B, C;
+	
+	scanf_s("%ld %ld %ld", &A, &B, &C);
+	printf("%ld\n", A + B + C);
+
+	
+	
 	return 0;
-
-
 }
